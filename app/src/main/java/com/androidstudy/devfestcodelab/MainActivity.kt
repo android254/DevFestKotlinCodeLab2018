b8a816aa2fb384c2fbfef952e1d6a130d2d833e8
Juma Allan <allanjuma@gmail.com>
2018-09-22 12:22:26 +0300

    Updated Demo

--- app/src/main/java/com/androidstudy/devfestcodelab/MainActivity.kt
+++ app/src/main/java/com/androidstudy/devfestcodelab/MainActivity.kt
@@ -2,11 +2,14 @@ package com.androidstudy.devfestcodelab
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.text = "Hello World!"
     }
 }
